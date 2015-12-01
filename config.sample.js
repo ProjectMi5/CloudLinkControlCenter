71ba06847f9c00cab429258a3316fe90cb8cc2e2
Thomas Frei <mail@thomasfrei.de>
2015-12-01 15:47:53 +0100

    update gitignore and sample config

--- config.sample.js
+++ config.sample.js
@@ -3,11 +3,17 @@
  */
 var config = {};
 
-config.rest = {};
-config.rest.host = 'https://foo.bar.xz';
-config.rest.getTasks = 'getTasks';
 config.auth = {}; // used with request - keep object properties user and password!
 config.auth.user = 'foo';
 config.auth.password = 'bar';
+	
+config.rest = {};
+config.rest.host = 'https://mi5.itq.de';
+config.rest.getTasks = 'getTasks';
+config.rest.getOrdersByStatus = 'getOrdersByStatus';
+config.rest.updateOrderStatus = 'updateOrderStatus';
+
+config.order = {};
+config.order.validStatus = ['pending', 'rejected', 'accepted', 'in progress', 'done', 'delivered', 'archived', 'aborted', 'failure']
 
 module.exports = config;
